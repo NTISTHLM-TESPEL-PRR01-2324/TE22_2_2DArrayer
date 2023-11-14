c3b65c99233bd89d2976f91c76c7f9cdffe27ebb
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2023-11-14 10:18:10 +0100

    Initial commit

---
+++ TE22_2_2DArrayer/Program.cs
@@ -0,0 +1,30 @@
+﻿int[,] map = new int[10, 10];
+
+
+map[1, 0] = 1;
+
+int count = 0;
+
+for (int y = 0; y < map.GetLength(1); y++)
+{
+  for (int x = 0; x < map.GetLength(0); x++)
+  {
+    Console.Write(map[x, y]);
+    count++;
+  }
+  Console.WriteLine();
+}
+
+Console.WriteLine("\n" + count);
+
+
+// char[] letters = { 'X', 'O', 'O', 'O', 'O', 'O', 'X' };
+
+// for (int i = 0; i < map.Length; i++)
+// {
+//   Console.Write(map[i]);
+// }
+
+
+
+Console.ReadLine();
